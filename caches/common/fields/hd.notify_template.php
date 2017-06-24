2a296900e998b5125fc444e7c85874fc793e2f84
lujiang <HR@51ao.com>
2017-06-24 15:37:27 +0800

    修改插件名

---
+++ caches/common/fields/hd.notify_template.php
@@ -0,0 +1,3 @@
+<?php
+//000000000000a:7:{i:0;s:2:"id";i:1;s:7:"enabled";i:2;s:8:"template";i:3;s:4:"name";s:8:"_autoinc";b:0;s:3:"_pk";s:2:"id";s:5:"_type";a:4:{s:2:"id";s:12:"varchar(100)";s:7:"enabled";s:4:"text";s:8:"template";s:4:"text";s:4:"name";s:12:"varchar(100)";}}
+?>
